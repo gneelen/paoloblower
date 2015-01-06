93a5d783097442c6945b2c68e785a6fd44fcdaef
Jeremia Kimelman <jbkimelman@gmail.com>
2015-01-05 19:56:07 -0800

    google spreadsheet functions

--- js/custom.js
+++ js/custom.js
@@ -15,6 +15,7 @@ $(document).ready(function() {
 function init() {
   onePageScroll();
   addMap();
+  getDataFromGoogleSpreadsheet();
 }
 
 /* --- Full Screen Container ------------- */
@@ -55,6 +56,8 @@ window.scrollReveal = new scrollReveal();
 
 /* --- One Page Scroll ------------------- */
 
+
+
 function onePageScroll() {
   $('.nav').onePageNav({
       currentClass: 'current',
@@ -74,6 +77,37 @@ function onePageScroll() {
           //I get fired when you enter a section and I pass the list item of the section
       }
   });
+}
+
+function addAboutParagraphs(paragraphs) {
+
+}
+
+function addTestimonial(testimonialData) {
+  var templateText = $('#testimonialTemplate').text().trim();
+  var testimonial = $('<div/>').html(templateText).contents();
+
+  testimonial.find('p').text(testimonialData['text']);
+  testimonial.find('.name').text(testimonialData['name']);
+  testimonial.find('.company').text(testimonialData['company']);
+
+  $('#testimonialList').append(testimonial);
+}
+
+function processSpreadsheetData(data, tabletop) {
+  console.log("Successfully processed!")
+  console.log('data', data);
+
+  addAboutParagraphs(data['About']['elements']);
+
+  for (var t=0;t<data['Testimonials']['elements'].length;t++) {
+    addTestimonial(data['Testimonials']['elements'][t]);
+  }
+}
+
+function getDataFromGoogleSpreadsheet() {
+  var docKey = 'https://docs.google.com/spreadsheet/pub?hl=en_US&hl=en_US&key=15RKaYrdMGn-FSglHi4_QBKHVgiF_VwF7seg2jg6epyU&output=html';
+  Tabletop.init( {key: docKey, callback: processSpreadsheetData, debug:true} );
 }
 
 function addMap() {
